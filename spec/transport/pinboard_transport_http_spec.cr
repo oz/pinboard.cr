7fcf5b0d9366a8898615ba3a85f718ad6ecd29b8
Arnaud Berthomier <oz@cypr.io>
2017-05-25 22:42:17 +0200

    Better tests

--- spec/transport/pinboard_transport_http_spec.cr
+++ spec/transport/pinboard_transport_http_spec.cr
@@ -3,13 +3,15 @@ require "../spec_helper"
 describe Pinboard::Transport::HTTP do
   transport = Pinboard::Transport::HTTP.new token: "test"
 
-  it "builds URL correctly" do
-    params = {
-      "foo" => "bar",
-      "bool" => false,
-      "array" => %w(foo bar baz)
-    }
-    url = transport.build_url("/test", params)
-    url.should eq("https://api.pinboard.in/v1/test?foo=bar&bool=no&array=foo%2Cbar%2Cbaz&format=json&auth_token=test")
+  describe "#build_url" do
+    it "converts a Hash param to a query URL" do
+      params = {
+        "foo" => "bar",
+        "bool" => false,
+        "array" => %w(foo bar baz)
+      }
+      url = transport.build_url("/test", params)
+      url.should eq("https://api.pinboard.in/v1/test?foo=bar&bool=no&array=foo%2Cbar%2Cbaz&format=json&auth_token=test")
+    end
   end
 end
